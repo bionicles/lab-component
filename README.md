3320d494e826d2fc2ba8a342f251d71a5247f6cf
Bion Howard <bion@bitpharma.com>
2019-05-20 19:08:20 -0400

    add lab and main

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+yarn add @jupyterlab/coreutils @jupyterlab/application @jupyterlab/application-extension @jupyterlab/apputils-extension @jupyterlab/codemirror-extension @jupyterlab/completer-extension @jupyterlab/console-extension @jupyterlab/csvviewer-extension @jupyterlab/docmanager-extension @jupyterlab/fileeditor-extension @jupyterlab/filebrowser-extension @jupyterlab/help-extension @jupyterlab/imageviewer-extension @jupyterlab/inspector-extension @jupyterlab/launcher-extension @jupyterlab/mainmenu-extension @jupyterlab/markdownviewer-extension @jupyterlab/mathjax2-extension @jupyterlab/notebook-extension @jupyterlab/rendermime-extension @jupyterlab/running-extension @jupyterlab/settingeditor-extension @jupyterlab/shortcuts-extension @jupyterlab/tabmanager-extension @jupyterlab/terminal-extension @jupyterlab/theme-dark-extension @jupyterlab/theme-light-extension @jupyterlab/tooltip-extension
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+const startJupyterLab = () => {
+  require("font-awesome/css/font-awesome.min.css");
+  var JupyterLab = require("@jupyterlab/application")
+    .JupyterLab;
+
+  var mods = [
+    require("@jupyterlab/application-extension"),
+    require("@jupyterlab/apputils-extension"),
+    require("@jupyterlab/codemirror-extension"),
+    require("@jupyterlab/completer-extension"),
+    require("@jupyterlab/console-extension"),
+    require("@jupyterlab/csvviewer-extension"),
+    require("@jupyterlab/docmanager-extension"),
+    require("@jupyterlab/fileeditor-extension"),
+    require("@jupyterlab/filebrowser-extension"),
+    require("@jupyterlab/help-extension"),
+    require("@jupyterlab/imageviewer-extension"),
+    require("@jupyterlab/inspector-extension"),
+    require("@jupyterlab/launcher-extension"),
+    require("@jupyterlab/mainmenu-extension"),
+    require("@jupyterlab/markdownviewer-extension"),
+    require("@jupyterlab/mathjax2-extension"),
+    require("@jupyterlab/notebook-extension"),
+    require("@jupyterlab/rendermime-extension"),
+    require("@jupyterlab/running-extension"),
+    require("@jupyterlab/settingeditor-extension"),
+    require("@jupyterlab/shortcuts-extension"),
+    require("@jupyterlab/tabmanager-extension"),
+    require("@jupyterlab/terminal-extension"),
+    require("@jupyterlab/theme-dark-extension"),
+    require("@jupyterlab/theme-light-extension"),
+    require("@jupyterlab/tooltip-extension")
+  ];
+  var lab = new JupyterLab({
+    name: "JupyterLab Example",
+    namespace: "lab-example",
+    version: require("../package.json").version,
+    hostId: "lab"
+  });
+  lab.registerPluginModules(mods);
+  lab.start().then(() => {
+    // eslint-disable-next-line
+    console.log("Example started!");
+  });
+}
+
+const App = () => {
+  useEffect(startJupyterLab, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test</h1>
+      <div id="lab" />
     </div>
   );
 }
